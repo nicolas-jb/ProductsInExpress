6a14ed5a8a3c679362896ed194b7a057ba53d95d
Nicolas <nicolas.bernardelli@gmail.com>
2021-10-17 03:06:08 -0300

    deliver 14 - global process - fork

---
+++ src/Entrega14/routerFork.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { Router } = express;
+const routerFork = new Router();
+const { fork } = require("child_process");
+
+const randoms = fork("randomsNumbers.js");
+
+routerFork.get("/", (req, res) => {
+  const cantidad = Number(req.query.cant) || 100e6;
+  randoms.on("message", (resultado) => {
+    return res.status(200).json(resultado);
+  });
+  randoms.send(cantidad);
+});
+
+module.exports = { routerFork };
---
+++ src/Entrega14/server.js
@@ -0,0 +1,180 @@
+/* --------------------------------- Imports -------------------------------- */
+
+const express = require("express");
+const handlebars = require("express-handlebars");
+const emoji = require("node-emoji");
+const { generateFakeProducts } = require("./productosFake.js");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const session = require("express-session");
+const parseArgs = require("minimist");
+const {routerFork} = require("./routerFork.js");
+
+/* -------------------------------------------------------------------------- */
+/* --------------------------------- Config --------------------------------- */
+
+dotenv.config({ path: __dirname + "/.env" });
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos"],
+      scope: ["email"],
+    },
+    (accessToken, refreshToken, userProfile, done) => {
+      return done(null, userProfile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((id, done) => {
+  done(null, id);
+});
+
+const app = express();
+const args = parseArgs(process.argv.slice(2));
+const PORT = args._[0] || 8080;
+const MAXAGE = 10 * 60 * 1000; //10 minutes in ms
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    rolling: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: MAXAGE,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/randoms", auth, routerFork);
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------- Middleware de control ------------------------- */
+
+async function auth(req, res, next) {
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.render("login_alert");
+  }
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/* ------------------------------- Handlebars ------------------------------- */
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    // eslint-disable-next-line no-undef
+    layoutsDir: __dirname + "/views/layouts",
+    // eslint-disable-next-line no-undef
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+app.set("view engine", "hbs");
+// eslint-disable-next-line no-undef
+app.set("views", __dirname + "/views");
+// eslint-disable-next-line no-undef
+app.use(express.static(__dirname + "/public"));
+
+/* -------------------------------------------------------------------------- */
+/* ----------------------------- FakerGenerator ----------------------------- */
+
+function initFakeProducts() {
+  const fakeProd = [];
+  for (let index = 0; index < 5; index++) {
+    fakeProd.push(generateFakeProducts());
+  }
+
+  return fakeProd;
+}
+
+let productosFake = initFakeProducts();
+
+/* -------------------------------------------------------------------------- */
+/* --------------------------------- Routes --------------------------------- */
+
+app.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/faillogin",
+  })
+);
+
+app.get("/faillogin", (req, res) => {
+  res.render("fail-login");
+});
+
+app.get("/logout", auth, (req, res) => {
+  const userName = req.user.displayName;
+  req.logout();
+  res.render("logout", { user: userName });
+});
+
+app.get("/", auth, (req, res) => {
+  res.status(200).render("main", {
+    data: productosFake,
+    userName: req.user.displayName,
+    userPhoto: req.user.photos[0].value,
+  });
+});
+
+app.post("/", auth, (req, res) => {
+  productosFake.push(generateFakeProducts());
+  res.status(200).render("main", {
+    data: productosFake,
+    user: req.session.user,
+  });
+});
+
+app.get("/info", auth, (req, res) => {
+  const folder = process.cwd().split("/").pop();
+  res.status(200).json({
+    "Argumentos de entrada": args._,
+    Plataforma: process.platform,
+    "Versión Node": process.version,
+    "Memoria total reservada (rss)": process.memoryUsage().rss,
+    "Path de ejecución": process.cwd(),
+    "Process id": process.pid,
+    "Carpeta del proyecto": folder,
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------- Server On ------------------------------- */
+
+app.listen(PORT, () => {
+  console.log(`${emoji.get("computer")} Servidor activo en ${PORT}`);
+});
+
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------- Export ------------------------------- */
+
+module.exports = { auth };
